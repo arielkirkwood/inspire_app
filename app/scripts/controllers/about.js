94d379c08fd2da114babcfd6cf8cd5dca91c54c4
Ariel Kirkwood <ariel.kirkwood@me.com>
2017-01-19 11:18:21 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inspireAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inspireAppApp
+ */
+angular.module('inspireAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
