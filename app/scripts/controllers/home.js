47194e7a8de0b39675ae2f5bb7d68b4d0df6ad77
Ariel Kirkwood <ariel.kirkwood@me.com>
2017-01-20 21:51:01 -0500

    Extract sample data to sample service

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,12 +8,9 @@
  * Controller of the inspireAppApp
  */
 angular.module('inspireAppApp')
-  .controller('HomeCtrl', ['$scope', function ($scope) {
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
+  .controller('HomeCtrl', ['$scope', 'HomeService', function ($scope, HomeService) {
+    $scope.labels = HomeService.labels;
+    $scope.series = HomeService.series;
 
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+    $scope.data = HomeService.data;
   }]);
